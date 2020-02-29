7507fabe0c38b4958d59ad349d509c1209ce14a7
James Pooley <james.pooley@gmail.com>
2020-02-29 14:57:16 -0500

    Update setup with one of Vince Buffalo's excellent ideas

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+set -eu
+
+
+# Taken from https://github.com/vsbuffalo/dotfiles/blob/master/setup.sh
+function linkdotfile {
+  file="$1"
+  if [ ! -e ~/$file -a ! -L ~/$file ]; then
+    echo "$file not found, linking..." >&2
+    ln -s ~/dotfiles/$file ~/$file
+  else
+    echo "$file found, ignoring..." >&2
+  fi
+}
+
+
+linkdotfile .tmux.conf
